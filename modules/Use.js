bcf053f449c35f0822c657277350e16d470a548a
Abhipal7500 <131875068+Abhipal7500@users.noreply.github.com>
2024-06-23 18:48:49 +0530

    user model added

---
+++ modules/Use.js
@@ -0,0 +1,39 @@
+const moment = require('moment');
+const mongoose = require('mongoose');
+const Schema= mongoose.Schema;
+
+const userSchema=new Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    email:{
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profile_pic:{
+        type: String,
+        default:"empty-avatar.jpg"
+    },
+    createdAt:{
+        type:Date,
+        default: moment().format("DD/MM/YYYY") + ";" + moment().format("hh:mm:ss")
+    },
+
+    updatedAt:{
+        type:Date,
+        default: moment().format("DD/MM/YYYY") + ";" + moment().format("hh:mm:ss")
+    }
+
+});
+
+mongoose.model("users",userSchema);
+
+module.exports = mongoose.model("users");
+
+
+
